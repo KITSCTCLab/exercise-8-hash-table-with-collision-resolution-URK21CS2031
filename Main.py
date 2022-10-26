7c494d74b9f882c133537839f7172777760b2b11
URK21CS2031 <109648188+URK21CS2031@users.noreply.github.com>
2022-10-26 09:54:16 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -20,8 +20,8 @@ HashTable = [[] for _ in range(hash_table_size)]
 input_data = input()
 data = []
 for item in re.split('], |].', input_data):
- item = item[1:]
- data = item.split(', ')
- if len(data) > 1:
- insert(HashTable, int(data[0]), data[1])
+      item = item[1:]
+      data = item.split(', ')
+      if len(data) > 1:
+        insert(HashTable, int(data[0]), data[1])
 display_hash (HashTable)
